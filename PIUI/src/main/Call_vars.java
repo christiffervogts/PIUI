1f7e05428f0780e829578f43eeb1b31b10af463f
christiffervogts <christiffer.vogts221@gmail.com>
2026-04-26 23:50:23 -0400

    added most of Call_vars

--- PIUI/src/main/Call_vars.java
+++ PIUI/src/main/Call_vars.java
@@ -1,5 +1,46 @@
 package main;
 
+import java.io.*;
+
 public class Call_vars {
 
+	public String user;
+	public String location;
+	public String password;
+	
+	BufferedReader br;
+	BufferedWriter bw;
+	
+	public Call_vars() {
+		if(!check_for_save()) {
+			make_save();
+		}
+	}
+	public boolean check_for_save() {
+		try {
+			br = new BufferedReader(new FileReader("Saves.txt"));
+			
+			try {
+				user = br.readLine();
+				location = br.readLine();
+				password = br.readLine();
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			return false;
+		}
+		return false;
+	}
+	public void make_save() {
+		try {
+			bw = new BufferedWriter(new FileWriter("Saves.txt"));
+			//find a way to get this in a window.
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
